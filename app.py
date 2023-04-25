87732a40a0a38f55d26abbbd26fc109ae261ec4f
Moez Walha <74401465+Moezwalha@users.noreply.github.com>
2023-04-25 17:09:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,30 @@ import base64
 from PIL import Image
 from io import BytesIO
 
+def get_net():
+    finetune_net = nn.Sequential()
+    finetune_net.features = models.resnet18(weights='ResNet18_Weights.DEFAULT')
+
+
+    finetune_net.output_new = nn.Sequential(nn.Linear(1000, 256),
+                                            nn.ReLU(),
+                                            nn.Linear(256, 26))
+
+    finetune_net = finetune_net.to('cpu')
+
+    for param in finetune_net.features.parameters():
+        param.requires_grad = False
+    return finetune_net
+
+
+# Load the saved parameters
+saved_params = torch.load('my_model61.pt', map_location=torch.device('cpu'))
+
+# Create a new instance of the model and load the parameters
+model_test = get_net()
+model_test.load_state_dict(saved_params)
+model_test.eval()
+classes = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 app = Flask(__name__)
 
