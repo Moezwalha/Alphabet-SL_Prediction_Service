12f90ee92ba75bc715972a854ca24979a4c4f717
Moez Walha <74401465+Moezwalha@users.noreply.github.com>
2023-04-25 20:50:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,25 @@ import base64
 from PIL import Image
 from io import BytesIO
 from preprocess4 import Pr1
-from preprocess2 import test_transforms
 import torch
 from torchvision import models
 import torch.nn as nn
 import numpy as np
 import cv2
+import torchvision.transforms as transforms
+
+test_transforms = transforms.Compose([
+        
+    # resize the image to 224x224 pixels
+    transforms.Resize((224, 224)),
+    
+    # convert the image to a PyTorch tensor
+    transforms.ToTensor(),
+        
+    # normalize the tensor by subtracting the mean and dividing by the standard deviation of each color channel
+    transforms.Normalize([0.5189, 0.4991, 0.5138],
+                             [0.2264, 0.2539, 0.2625])
+])
 
 def get_net():
     finetune_net = nn.Sequential()
