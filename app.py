dd3d3eaff4f831dd8af7332794ea4dac8068540c
Moez Walha <74401465+Moezwalha@users.noreply.github.com>
2023-04-26 02:03:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, request
 import base64
 from PIL import Image
 from preprocess4 import Pr1
+from preprocess2 import test_transforms
 import torch
 from torchvision import models
 import torch.nn as nn
 import numpy as np
 import cv2
-from torchvision.transforms import Resize
 
 def get_net():
     finetune_net = nn.Sequential()
@@ -34,6 +34,7 @@ model_test.load_state_dict(saved_params)
 model_test.eval()
 classes = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+
 app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
@@ -49,46 +50,26 @@ def upload():
         
         # Convert the image bytes to a numpy array
         nparr = np.fromstring(image_bytes, np.uint8)
-        print(3)
+        
         # Decode the numpy array to an image using OpenCV
         frame = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
-        print(4)
         
         # Process the image as needed
         p1 = Pr1(frame)
-        print(5)
         processed_frame = p1.detect_crop_and_segment_hands(p1.image)
-        print(6)
         if processed_frame is not None: 
-            print(7)
-            cropped_hand_array = Image.fromarray(processed_frame)
-            print(cropped_hand_array)
-            print(8)
-            
-            # Resize the image to a specific size
-            resize = Resize((224, 224))
-            print(8.5)
-            cropped_hand_pil = resize(cropped_hand_array)
-            print(cropped_hand_pil)
-            print(type(cropped_hand_pil))
-            print(9)
-            # Convert PIL image to NumPy array
-            cropped_hand_array = np.array(cropped_hand_pil)
-            print(9.25)
-            # Convert NumPy array to PyTorch tensor
-            cropped_hand_tensor = torch.from_numpy(cropped_hand_array)
-            print(9.5)
-            #Make a prediction using the model
-            prediction = model_test(cropped_hand_tensor[None].to("cpu")) 
-            print(10)
-            # Get the predicted label
-            pred_label = classes[torch.max(prediction, dim=1)[1]]
-            print(pred_label)
+                cropped_hand_array = Image.fromarray(processed_frame)
+                # Apply the transformations
+                img_tensor = test_transforms(cropped_hand_array)
+                #Make a prediction using the model
+                prediction = model_test(img_tensor[None].to("cpu"))            
+                # Get the predicted label
+                pred_label = classes[torch.max(prediction, dim=1)[1]]
+                print(pred_label)
+        # Return a response if needed
         return {'status': 'success'}
-    
     except Exception as e:
         return {'status': 'error', 'message': str(e)}
-
 
 if __name__ == '__main__':
     app.run()
